977b5c509ef828e6a967b25710b6cd7fae3f38e9
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-05-07 22:22:55 +1000

    Add site favicons

--- templates/resources.php
+++ templates/resources.php
@@ -1,4 +1,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="images/icon-16.png" sizes="16x16"/>
+<link rel="icon" type="image/png" href="images/icon-48.png" sizes="48x48"/>
+<link rel="icon" type="image/png" href="images/icon-192.png" sizes="192x192"/>
+<link rel="shortcut icon" href="images/icon.ico"/>
 <link rel="stylesheet" type="text/css" href="styles/global.scss"/>
 <script type="text/javascript" src="libraries/jquery.js"></script>
 <script type="text/javascript" src="script/global.js"></script>
