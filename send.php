955128aef93ce0e464d542079f07f51a453c29ca
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-05-07 22:13:15 +1000

    Add contact details to end of delivered email

--- send.php
+++ send.php
@@ -3,12 +3,14 @@
 	$email = $_POST["email"];
 	$phone = $_POST["phone"];
 	$subject = "Message from greenfinchgardening.co.uk";
-	$message = stripslashes($_POST["message"] . "\n\n" . $phone);
+	$message = $_POST["message"];
+
+	$message .= "\n\nFrom: " . $name . "\nPhone: " . $phone . "\nEmail: ". $email;
 	
 	$to = "contact+gg@rocketchilli.com";
 	$headers = "From: " . $name . " <" . $email . ">";
 	
-	if (mail($to, $subject, $message, $headers))
+	if (mail($to, $subject, stripslashes($message), $headers))
 		http_response_code(201);
 	else
 		http_response_code(400);
