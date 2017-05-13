8478263b78ab1767e69c7dbf469e677b1e40157d
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-05-13 13:50:02 +1000

    Start working on About page

---
+++ about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>About Greenfinch Gardening</title>
+	<meta name="description" content="") ?>
+	<?php include("templates/resources.php") ?>
+</head>
+<body>
+	<header class="inverted">
+		<?php include("templates/header.php") ?>
+		<h1>About me</h1>
+		<hr/>
+		<div class="logo-text"><em>G</em>reenfinch <em>G</em>ardening</div>
+	</header>
+	<section>
+		<p>I'll be honest, this isn't my first career. With a life-long love of nature and a degree in Biology, my first job after leaving university was making wildlife films for the BBC Natural History Unit in Bristol. For the best part of ten years, my working week was spent researching for iconic programmes such as the Natural World and Springwatch, while evenings and weekends were filled with producing nature-inspired paintings for exhibitions in galleries across the UK.</p>
+		<p>With our move to the Chew Valley in 2011, came the opportunity to apply both my knowledge of wildlife and artistic flair in a different direction - that of turning our newly acquired garden into something both my family and the local wildlife could enjoy. In short it became an obsession, culminating in a book written by my husband, the Naturalist &amp; Broadcaster Mike Dilger, and illustrated by me, entitled ‘My Garden and Other Animals'. The book charted the trials and tribulations of converting a long-abandoned back yard into firstly an aesthetically beautiful garden and secondly a miniature nature reserve for a whole range of birds, bees and butterflies.</p>
+	</section>
+	<section class="inverted">
+		<p>I loved making nature documentaries, however, with the arrival of my son, everything changed. Balancing work and life as a busy mother, I suddenly found sitting at dawn in the back-of-beyond less appealing and working closer to home a far more attractive proposition. Fast-forward five years and I'm now an RHS qualified Gardener with a considerable amount of horticultural experience under my belt. In essence I'm never happier than when wielding my secateurs.</p>
+		<p>I strongly feel that nature friendly gardens need not be messy and that gardening without chemicals is the way forward when controlling pests. Our gardens are a microcosm of the natural world, which is why many of us cherish them, so interfering less in complex ecosystems that we barely understand, will in the long term be to the benefit of both the environment and of course our garden.</p>
+		<p>I'm passionate about plants and really enjoy meeting and collaborating with clients, to help them turn their gardens from a corner hidden away into something worth showing off. Wether it be getting stuck in with a spade or using my horticultural knowledge to advise clients on the most suitable plants and planting schemes for their garden and lifestyle, I believe I can make a real difference.</p>
+		<?php include("templates/footer.php") ?>
+	</section>
+	<?php include("templates/defer.php") ?>
+</body>
+</html>
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <nav class="full-width">
-	<a class="active" href="">Home</a>
+	<a class="active" href="/">Home</a>
 	<a href="#services">Services</a>
-	<a href="">About</a>
+	<a href="about">About</a>
 	<a href="#contact">Contact</a>
 </nav>
