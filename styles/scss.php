1b63cb6fb682e952980391b3cb66979a956f468e
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-06-12 20:49:24 +1000

    Enable compression and caching

--- styles/scss.php
+++ styles/scss.php
@@ -3,10 +3,15 @@
 	use Leafo\ScssPhp\Compiler;
 	use Leafo\ScssPhp\Server;
 
+	// Enable gzip compression of output
+	ob_start("ob_gzhandler");
+
+	// Create compiler
 	$scss = new Compiler();
 	if (array_key_exists("min", $_GET))
 		$scss->setFormatter("Leafo\ScssPhp\Formatter\Crunched");
 
+	// Start server
 	$directory = ".";
 	$server = new Server($directory, null, $scss);
 	$server->serve();
